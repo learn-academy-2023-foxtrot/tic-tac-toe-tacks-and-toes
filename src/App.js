55a98c6d5884b9e1d4a412f4ba6240ed7a6e3cb1
tori-calkins <tori.calkins2705@gmail.com>
2023-09-19 10:23:32 -0700

    contional statement started

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill("💯"))
-  const handleClick = (select) => {
-  if(select % 2 === 0){
-    setSquares = "❌"
-    
-  } else {
-    setSquares = "⭕️"
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const handleClick = (selectedIndex) => {
+    console.log(selectedIndex)
+  if(squares[selectedIndex] !== "⭕️" || squares[selectedIndex] === null) {
+      squares[selectedIndex] = "❌"
+      setSquares([...squares])
+  }else if(squares[selectedIndex] !== "❌" && squares[selectedIndex] === null) {
+    squares[selectedIndex] = "⭕️"
+    setSquares([...squares])
   }
+ //if(squares[selectedIndex] !== "❌" && squares[selectedIndex] === null) 
   } 
   return (
     <>
